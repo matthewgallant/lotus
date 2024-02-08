dd47638acbebf82e0e09ed8795c99209878c7a97
Matthew Gallant <matt@matthewgallant.me>
2024-02-07 19:05:43 -0500

    Fix data and add more search options

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import urllib.parse
+
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -18,6 +20,11 @@ class Cards(db.Model):
     collector_number: Mapped[int] = mapped_column()
     scryfall_id: Mapped[str] = mapped_column()
     color_identity: Mapped[str] = mapped_column()
+    type_line: Mapped[str] = mapped_column()
+    cmc: Mapped[int] = mapped_column()
+    power: Mapped[int] = mapped_column()
+    toughness: Mapped[int] = mapped_column()
+    rarity: Mapped[str] = mapped_column()
 
 # Create the app
 app = Flask(__name__)
@@ -32,19 +39,30 @@ def handle_search():
     query = db.select(Cards).group_by(Cards.name)
 
     # Handle color searches
+    if request.args.get('name'):
+        query = query.where(Cards.name.like(f'%{request.args.get("name")}%'))
     if request.args.get('color'):
         colors = request.args.get('color').split(',')
-        for color in colors:
-            if color != 'un':
-                query = query.where(Cards.color_identity.contains(color))
-            else:
-                query = query.where(Cards.color_identity == None)
+        core_colors = ['w', 'u', 'b', 'r', 'g']
+        if 'un' in colors:
+            query = query.where(Cards.color_identity == None, Cards.type_line.notlike('land'))
+        else:
+            for color in core_colors:
+                if color in colors:
+                    query = query.where(Cards.color_identity.contains(color))
+                else:
+                    query = query.where(Cards.color_identity.notlike(f'%{color}%'))
+    if request.args.get('type'):
+        query = query.where(Cards.type_line.like(f'%{request.args.get("type")}%'))
+    
+    print(query)
     
     return db.paginate(query, per_page=12)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    collection_total = db.session.query(Cards).count()
+    return render_template("home.html", collection_total=collection_total)
 
 @app.route("/search")
 def search():
@@ -53,16 +71,37 @@ def search():
 @app.route("/results", methods=['GET', 'POST'])
 def results():
     if request.method == 'POST':
-        # Build GET url and then redirect
-        url = '/results?'
+        # Process POST data
+        params = {}
+        if request.form.get('name'):
+            params['name'] = request.form.get('name')
         if len(request.form.getlist('color')) > 0:
-            url += f'color={",".join(request.form.getlist("color"))}'
-        return redirect(url)
+            params['color'] = ",".join(request.form.getlist("color"))
+        if request.form.get('type'):
+            params['type'] = request.form.get('type')
+        
+        # Redirect to GET url
+        return redirect(f'/results?{urllib.parse.urlencode(params)}')
     else:
         cards = handle_search()
-        return render_template("results.html", cards=cards)
+
+        # Redirect to card page if only 1 card is found
+        if cards.total == 1:
+            return redirect(f'/card/{cards.items[0].id}')
+        else:
+            return render_template("results.html", cards=cards)
 
 @app.route("/api/cards")
 def api_cards():
     cards = handle_search()
     return render_template("api/cards.html", cards=cards)
+
+@app.route("/card/<id>")
+def card(id):
+    card = db.get_or_404(Cards, id)
+    cards = db.session.execute(db.select(Cards).where(Cards.name == card.name)).scalars().all()
+    return render_template("card.html", card=card, cards=cards)
+
+@app.errorhandler(404) 
+def not_found(error):
+    return render_template("404.html")
--- utilities/mtggoldfish2lotus.py
+++ utilities/mtggoldfish2lotus.py
@@ -3,6 +3,7 @@ import csv
 import sqlite3
 import requests
 import json
+import time
 import urllib.parse
 
 # Collect arguments
@@ -12,7 +13,7 @@ parser.add_argument('-db', help='SQLite database file with a configured cards ta
 args = parser.parse_args()
 
 # Open DB connection
-con = sqlite3.connect(args.sqlite)
+con = sqlite3.connect(args.db)
 cur = con.cursor()
 
 # Open CSV reader
@@ -21,43 +22,119 @@ reader = csv.reader(args.csv, delimiter=',')
 # Skip header row
 next(reader, None)
 
+# Collect errors
+errors = []
+
 for row in reader:
-    # Parse each column
+    # Parse each column that we care about
     name = row[0]
     set_id = row[1]
-    set_name = row[2]
     quantity = int(row[3])
     foil = row[4]
     variation = row[5]
     collector_number = row[6]
     scryfall_id = row[7]
 
-    print(f'Importing {name}')
+    # Set null defaults
+    if name == '':
+        name = None
+    if set_id == '':
+        set_id = None
+    if quantity == '':
+        quantity = None
+    if foil == '':
+        foil = None
+    if variation == '':
+        variation = None
+    if collector_number == '':
+        collector_number = None
+    if scryfall_id == '':
+        scryfall_id = None
 
-    # Gather additional details from Scryfall
+    print(f'Importing: {name}')
+
+    # Set null defaults for Scryfall data
     color_identity = None
+    type_line = None
+    cmc = None
+    power = None
+    toughness = None
+    rarity = None
 
+    # Attempt to locate card on Scryfall
     if scryfall_id:
         res = requests.get(f'https://api.scryfall.com/cards/{scryfall_id}')
     elif set_id and collector_number:
-        res = requests.get(f'https://api.scryfall.com/cards/{set_id}/{collector_number}')
+        res = requests.get(f'https://api.scryfall.com/cards/{set_id.lower()}/{collector_number}')
+    elif set_id:
+        res = requests.get(f'https://api.scryfall.com/cards/named?fuzzy={urllib.parse.quote_plus(name)}&set={set_id.lower()}')
     else:
         res = requests.get(f'https://api.scryfall.com/cards/named?fuzzy={urllib.parse.quote_plus(name)}')
     
+    # Handle not finding the card
+    if res.status_code != 200:
+        print(f"There was an error finding '{name}' on Scryfall. Please enter that card's Scryfall ID to continue.")
+        scryfall_id = input("Scryfall ID: ")
+        res = requests.get(f'https://api.scryfall.com/cards/{scryfall_id}')
+        if res.status_code != 200:
+            errors.append(json.loads(res.text))
+            print("Still can't find card data, skipping...")
+            
+            # Prevent inserting row if we can't find a card but still have a Scryfall id
+            scryfall_id = None
+    
+    # Load data into dictionary
     data = json.loads(res.text)
     
+    # Parse data from Scryfall
     if 'id' in data:
         scryfall_id = data['id']
     if 'color_identity' in data:
-        if data['color_identity'] != '':
+        if data['color_identity'] != []:
             color_identity = ",".join(data['color_identity'])
+    if 'type_line' in data:
+        type_line = data['type_line']
+    if 'cmc' in data:
+        cmc = data['cmc']
+    if 'power' in data:
+        power = data['power']
+    if 'toughness' in data:
+        toughness = data['toughness']
+    if 'rarity' in data:
+        rarity = data['rarity']
+    if 'set' in data:
+        set_id = data['set'].upper()
+    if 'collector_number' in data:
+        collector_number = data['collector_number']
 
     # Insert new row
     if scryfall_id:
         cur.execute(
-            "INSERT INTO cards (name, set_id, quantity, foil, variation, collector_number, scryfall_id, color_identity) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-            (name, set_id, quantity, foil, variation, collector_number, scryfall_id, color_identity)
+            """
+            INSERT INTO cards (
+                name,
+                set_id,
+                quantity,
+                foil,
+                variation,
+                collector_number,
+                scryfall_id,
+                color_identity,
+                type_line,
+                cmc,
+                power,
+                toughness,
+                rarity
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            (name, set_id, quantity, foil, variation, collector_number, scryfall_id, color_identity, type_line, cmc, power, toughness, rarity)
         )
+    else:
+        print(f"No Scryfall ID found for {name}")
+        errors.append(data)
+    
+    # Delay requests per Scryfall's rules
+    time.sleep(0.1)
 
 # Commit and clean up
 con.commit()
