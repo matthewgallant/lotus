cd53dd59c73b90f5659fd64d4f9b840e7dc21900
Matthew Gallant <matt@matthewgallant.me>
2024-02-29 18:51:30 -0500

    Initial work on Docker image

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    LOTUS_ENVIRONMENT = os.environ.get('LOTUS_ENVIRONMENT')
+    if LOTUS_ENVIRONMENT == 'prod':
+        SQLALCHEMY_DATABASE_URI = "sqlite:////database/collection.db"
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
